c1e1581c879b0e24fd7cfcb4b317a58ee729a1db
Jonathan Velazquez <jvelazquez@nearsoft.com>
2018-07-23 11:57:26 -0500

    JRLateralAnimator

---
+++ JRHamburgerMenu/JRHamburgerMenu/JRLateralAnimator.swift
@@ -0,0 +1,10 @@
+//
+//  JRLateralAnimator.swift
+//  JRHamburgerMenu
+//
+//  Created by Israel Velazquez on 7/23/18.
+//  Copyright © 2018 Israel Jonathan Velazquez Sanchez. All rights reserved.
+//
+
+import Foundation
+import UIKit
